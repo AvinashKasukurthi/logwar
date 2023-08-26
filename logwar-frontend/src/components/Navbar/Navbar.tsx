08020e8bf7c100f24c4baeb78dfcbc76b3a4c9b5
Avinash <Avinash@zopnote.com>
2023-08-26 16:50:54 +0530

    logwar initial

---
+++ logwar-frontend/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <div className="m-4 flex justify-between">
+      <h2>Logwar</h2>
+      <>
+        <p>Products</p>
+      </>
+    </div>
+  );
+}
+
+export default Navbar;
